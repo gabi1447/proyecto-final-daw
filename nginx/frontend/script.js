b65ee72abccfc221bdbb3757cc9c559fa3922752
Gabriel Pacheco <gabrielpachecoyanezit@gmail.com>
2026-06-03 22:09:17 +0100

    Populate products on page load and when pagination triggers

--- nginx/frontend/script.js
+++ nginx/frontend/script.js
@@ -2,18 +2,36 @@ const categories = document.querySelectorAll(".category-button");
 const arrowLeft = document.getElementById("arrowLeft");
 const arrowRight = document.getElementById("arrowRight");
 const paginationInfo = document.getElementById("pageNumber");
+const productsContainer = document.getElementById("pagination");
 
 let currentPageNumber = 1;
 const totalPages = 10;
 
 let currentPageSelected = "1";
 
+function createProductCard(productData) {
+  return `
+        <div class="product-card">
+            <img src="${productData.image_url}" id="product-image" alt="${productData.name}">
+            <h3>${productData.name}</h3>
+            <p>$${productData.price}</p>
+            <a href="${productData.item_link}" target="_blank">Buy</a>
+        </div>
+    `;
+}
+
+function populateProducts(productsData) {
+  for (productData of productsData) {
+    productsContainer.innerHTML += createProductCard(productData);
+  }
+}
+
 function fetchProductData(category_id, page = 1, size = 10) {
   fetch(
     `http://localhost:8080/api/products/${category_id}?page=${page}&size=${size}`,
   )
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => populateProducts(data))
     .catch((error) => console.error("Error fetching products:", error));
 }
 
@@ -35,6 +53,7 @@ function updatePaginationInfo() {
 function loadEventListeners() {
   for (let category of categories) {
     category.addEventListener("click", () => {
+      productsContainer.innerHTML = "";
       currentPageSelected = category.id;
       currentPageNumber = 1;
       fetchProductData(category.id);
@@ -49,6 +68,7 @@ function loadPaginationEventListeners() {
     if (currentPageNumber === 1) {
       return;
     }
+    productsContainer.innerHTML = "";
     currentPageNumber -= currentPageNumber > 1 ? 1 : 0;
     fetchProductData(currentPageSelected, currentPageNumber);
     enable_disable_button();
@@ -58,6 +78,7 @@ function loadPaginationEventListeners() {
     if (currentPageNumber === 10) {
       return;
     }
+    productsContainer.innerHTML = "";
     currentPageNumber += currentPageNumber < 10 ? 1 : 0;
     fetchProductData(currentPageSelected, currentPageNumber);
     enable_disable_button();
